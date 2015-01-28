e5eca4abee99141c2f03d780267ab6de6a99b34a
wisewillwin <wisewillwin@gmail.com>
2015-01-27 20:42:12 -0500

    Second usage, have some problems in string.xml file.

--- app/src/main/java/com/learnandriod/hufeng/helloandriod/MainActivity.java
+++ app/src/main/java/com/learnandriod/hufeng/helloandriod/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +14,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+        Button firstButton = (Button) findViewById(R.id.button)
     }
 
 
